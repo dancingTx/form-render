583f76f14606b72ca105fa22986c174f2539cbe5
dancingTx <1150328467@qq.com>
2020-10-29 21:41:52 +0800

    feat(.travis.yml): modify master branch

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const resolve = url => path.resolve(__dirname, url)
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/form-render/': '',
+    ? '/form-render/' : '',
   chainWebpack (config) {
     config.plugins.delete('preload')
     config.plugins.delete('prefetch')
