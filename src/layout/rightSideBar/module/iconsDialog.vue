6e70ec922d129e15bdc91b0f0cae19d28e2a64e2
dancingTx <1150328467@qq.com>
2020-11-11 17:34:54 +0800

    feat(icons): add icon file

---
+++ src/layout/rightSideBar/module/iconsDialog.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-dialog
+      title="选择图标"
+      :close-on-click-modal="false"
+      :show-close="false"
+      v-on="$listeners"
+      v-bind="$attrs"
+      :visible.sync="dialogVisible"
+    >
+      <el-scrollbar>
+        <ul class="icons">
+          <li
+            :class="['icons__item', (activeIcon === icon) && 'icons__item--active' ]"
+            v-for="(icon, index) of elementIcons"
+            :key="index"
+            @click="handleSelected(icon)"
+          >
+            <i :class="icon"/>
+            <div>{{icon}}</div>
+          </li>
+        </ul>
+      </el-scrollbar>
+    </el-dialog>
+</template>
+
+<script>
+import elementIcons from '@/icons/__icons__'
+export default {
+  name: 'iconsDialog',
+  computed: {
+    elementIcons () {
+      return elementIcons.map(item => `el-icon-${item}`)
+    }
+  },
+  data () {
+    return {
+      activeIcon: ''
+    }
+  },
+  methods: {
+    handleSelected (icon) {
+      this.activeIcon = icon
+      this.dialogVisible = false
+    }
+  }
+}
+</script>
--- src/layout/rightSideBar/module/processIcons.vue
+++ src/layout/rightSideBar/module/processIcons.vue
@@ -4,24 +4,22 @@
       <el-button
         slot="append"
         icon="el-icon-thumb"
-        :close-on-click-modal="false"
-        :show-close="false"
         @click="dialogVisible = true"
       >
         选择图标
       </el-button>
     </el-input>
-    <el-dialog
-      title="提示"
-      :visible.sync="dialogVisible">
-      <span>这是一段信息</span>
-    </el-dialog>
+    <icons-dialog :visible.sync="dialogVisible"/>
   </div>
 </template>
 
 <script>
+import iconsDialog from './iconsDialog'
 export default {
   name: 'processIcons',
+  components: {
+    iconsDialog
+  },
   props: {
     conf: Object,
     option: Object
@@ -30,6 +28,8 @@ export default {
     return {
       dialogVisible: false
     }
+  },
+  methods: {
   }
 }
 </script>
