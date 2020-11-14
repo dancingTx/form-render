d7c1a3f7a96e685dc186f38696ceaf6447e970d9
dancingTx <1150328467@qq.com>
2020-11-14 19:38:48 +0800

    style(index.html): add loading animation

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,11 @@ import store from '@/store'
 Vue.use(VueRouter)
 const router = new VueRouter({ routes })
 router.afterEach(() => {
+  // 获取json
   store.dispatch('getComponents')
   store.dispatch('getFormConf')
+  // 取消加载动画
+  const spinner = document.querySelector('.spinner')
+  spinner.parentNode.removeChild(spinner)
 })
 export default router
